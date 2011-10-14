8b110cbd22f4e2e836c84b83c61d1b358de024c3
Ivo Strandzhev <istrandjev@google.com>
2011-10-14 20:49:43 +0200

    Fixing the missing tiles.

--- stone_tile.js
+++ stone_tile.js
@@ -2,5 +2,5 @@ function StoneTile() {
 }
 
 StoneTile.prototype.draw = function(container, x, y) {
-  Tile.draw(this, container, x, y, "tile2.png");
+  Tile.draw(this, container, x, y, "bg_gray.png");
 }
