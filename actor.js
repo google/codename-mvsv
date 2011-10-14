f379d76f5ad56e59f101be28219938c9ed41aeda
Sorin Stancu-mara <sstancumara@google.com>
2011-10-14 19:44:59 +0200

    Made nice move animation.

--- actor.js
+++ actor.js
@@ -4,6 +4,8 @@ function Actor() {
   this.speed = 0;
   this.maxSpeed = C.defaultMaxSpeed;
   this.drag = C.defaultDrag;
+  this.animTime = 0;
+  this.direction = 1;
 };
 
 Actor.prototype.accelerate = function(x) {
@@ -14,14 +16,17 @@ Actor.prototype.accelerate = function(x) {
   if (this.speed < -this.maxSpeed) {
     this.speed = -this.maxSpeed;
   }
+  if (this.speed < 0) this.direction = -1;
+  if (this.speed > 0) this.direction = 1;
 };
 
 Actor.prototype.tick = function(delta) {
-  this.x += this.speed / delta;
-  if (this.speed > this.drag / delta) {
-    this.speed -= this.drag / delta;
-  } else if (this.speed < -this.drag / delta) {
-    this.speed += this.drag / delta;
+  this.animTime += delta;
+  this.x += this.speed * delta;
+  if (this.speed > this.drag * delta) {
+    this.speed -= this.drag * delta;
+  } else if (this.speed < -this.drag * delta) {
+    this.speed += this.drag * delta;
   } else {
     this.speed = 0;
   }
