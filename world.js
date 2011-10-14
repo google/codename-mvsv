60d7e06735012e5639adb8fbe02514064d95e732
Ivo Strandzhev <istrandjev@google.com>
2011-10-14 21:39:52 +0200

    Remove unnecessary functions from world.

--- world.js
+++ world.js
@@ -17,23 +17,6 @@ World.prototype.draw = function(container) {
   }
 }
 
-
-World.prototype.setUpWorldClickHandlers = function() {
- for (var i = 0; i < this.tiles.length; i++) {
-    for (var j = 0; j < this.tiles[i].length; j++) {
-      this.tiles[i][j].node.addEventListener("click", this.tileClicked.bind(this, i, j)) 
-    }
-  }
- 
-}
-
-World.prototype.tileClicked = function(x, y) {
-  alert("you clicked" + x + " ||" + y);
-  for (var i = 0; i < this.actors.length; i++) {
-    this.actors[i].draw(container);
-  }
-} 
-
 World.prototype.loop = function() {
   var now = new Date().getTime() / 1000.0;
   var delta = now - this.lastLoop;
