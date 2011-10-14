4497825cee269fcaffeef8ece68561786c4d7305
Sorin Stancu-mara <sstancumara@google.com>
2011-10-14 18:15:05 +0200

    Fix the layout.

--- world.js
+++ world.js
@@ -6,7 +6,7 @@ function World() {
 World.prototype.draw = function(container) {
   for (var i = 0; i < this.tiles.length; i++) {
     for (var j = 0; j < this.tiles[i].length; j++) {
-      this.tiles[i][j].draw(container);
+      this.tiles[i][j].draw(container, i, j);
     }
   }
 } 
