0c443f50069949d07fcedf6848168207dc5a6e19
Sorin Stancu-mara <sstancumara@google.com>
2011-10-15 20:20:37 +0200

    Fix the collide.

--- actor.js
+++ actor.js
@@ -48,10 +48,12 @@ Actor.prototype.tick = function(delta, world) {
   // Don't get out of the world.
   if (Math.floor(this.x + this.width) >= world.tiles.length) {
     this.x = world.tiles.length - this.width - 0.0001;
+    colided = true;
     this.speed = 0;
   }
   if (this.x < 0) {
     this.x = 0;
+    colided = true;
     this.speed = 0;
   }
   if (Math.floor(this.y + this.height) >= world.tiles[0].length) {
@@ -61,6 +63,7 @@ Actor.prototype.tick = function(delta, world) {
   }
   if (this.y < 0) {
     this.y = 0;
+    colided = true;
     this.vspeed = 0;
   }
 
