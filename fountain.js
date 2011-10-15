636d35eae9aa4157ca2eacf5ace8258dfd3cf5d9
Ivo Strandzhev <istrandjev@google.com>
2011-10-15 15:41:30 +0200

    Add frozen images to water and fountains.

--- fountain.js
+++ fountain.js
@@ -6,3 +6,12 @@ FountainTile.prototype.draw = function(container, x, y, world) {
   name = "fountain.png";
   Tile.draw(this, container, x, y, name);
 }
+
+function FrozenFountainTile() {
+  this.passible = false;
+}
+
+FrozenFountainTile.prototype.draw = function(container, x, y, world) {
+  name = "fountain_frozen.png";
+  Tile.draw(this, container, x, y, name);
+}
--- water_tile.js
+++ water_tile.js
@@ -11,39 +11,39 @@ WaterTile.prototype.tick = function(delta) {
 }
 
 WaterTile.prototype.draw = function(container, x, y, world) {
-    var leftmost= false;
-    var rightmost = false;
-    if (x == 0 || Utils.getObjectClass(world.tiles[x-1][y]) != "WaterTile") {
-      leftmost = true;
+  var leftmost= false;
+  var rightmost = false;
+  if (x == 0 || (Utils.getObjectClass(world.tiles[x-1][y]) != "WaterTile" && Utils.getObjectClass(world.tiles[x-1][y]) != "FrozenWaterTile")) {
+    leftmost = true;
+  }
+  if (x + 1 == world.tiles.length || (Utils.getObjectClass(world.tiles[x+1][y]) != "WaterTile" && Utils.getObjectClass(world.tiles[x+1][y]) != "FrozenWaterTile")) {
+    rightmost = true;
+  }
+  if (leftmost) {
+    this.type = "water_left";
+  } else if (rightmost) {
+    this.type = "water_right";
+  } else {
+    this.type = "water_center";
+  }
+  above_fountain = false;
+  if (y + 1 != world.tiles[0].length && (Utils.getObjectClass(world.tiles[x][y + 1]) == "FountainTile"  || Utils.getObjectClass(world.tiles[x][y + 1]) == "FrozenFountainTile") ) {
+    above_fountain = true;
+  }
+  for (var i=0;i<C.neighbours.length; ++i) {
+    var tx = x + C.neighbours[i][0];
+    var ty = y + C.neighbours[i][1];
+    if (tx < 0 || tx >= world.tiles.length) {
+      continue;
     }
-    if (x + 1 == world.tiles.length || Utils.getObjectClass(world.tiles[x+1][y]) != "WaterTile") {
-      rightmost = true;
+    if (ty < 0 || ty >= world.tiles[0].length) {
+      continue;
     }
-    if (leftmost) {
-      this.type = "water_left";
-    } else if (rightmost) {
-      this.type = "water_right";
-    } else {
-      this.type = "water_center";
+    
+    if (Utils.getObjectClass(world.tiles[tx][ty]) == "WaterTile") {
+      this.time = world.tiles[tx][ty].time;
+      break;
     }
-    above_fountain = false;
-    if (y + 1 != world.tiles[0].length && Utils.getObjectClass(world.tiles[x][y + 1]) == "FountainTile" ) {
-      above_fountain = true;
-    }
-    for (var i=0;i<C.neighbours.length; ++i) {
-      var tx = x + C.neighbours[i][0];
-      var ty = y + C.neighbours[i][1];
-      if (tx < 0 || tx >= world.tiles.length) {
-	continue;
-      }
-      if (ty < 0 || ty >= world.tiles[0].length) {
-	continue;
-      }
-      
-      if (Utils.getObjectClass(world.tiles[tx][ty]) == "WaterTile") {
-	this.time = world.tiles[tx][ty].time;
-	break;
-      }
     }
   if (above_fountain) {
     this.type = "fountain_above";
