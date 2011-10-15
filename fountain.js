7442265a48357a7ece2f475f7be5a1dabe518bbb
Ivo Strandzhev <istrandjev@google.com>
2011-10-15 18:02:25 +0200

    Add melting ability

--- fountain.js
+++ fountain.js
@@ -18,12 +18,31 @@ FountainTile.prototype.draw = function(container, x, y, world) {
   }
   Tile.draw(this, container, x, y, name);
 }
+
 FountainTile.prototype.tick = function(delta) {
+  if (this.frost_.finished && this.frost_.freezing) {
+    this.frost_.stopFreezing();
+  }
+  if (this.frost_.finishedMelting && this.fire_.melting) {
+    this.fire_.stopMelting();
+  }
   this.frost_.tick(delta, this.x, this.y, this.world);
 }
 
 FountainTile.prototype.freeze = function() {
+  if (this.frozen) {
+    return;
+  }
   this.frozen = true;
   this.node.src = "gfx/fountain_frozen.png";
-  this.frost_.start();
+  this.frost_.startFreezing();
+}
+
+FountainTile.prototype.melt = function() {
+  if (!this.frozen) {
+    return;
+  }
+  this.frozen = false;
+  this.frost_.startMelting();
+  this.node.src = "gfx/fountain.png";
 }
