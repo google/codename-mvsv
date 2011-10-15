45bb0160f3f8b30b0bea093843b6aeeb6aa4d3a4
Ivo Strandzhev <istrandjev@google.com>
2011-10-15 15:13:44 +0200

    Adding fountain and level4

--- water_tile.js
+++ water_tile.js
@@ -7,24 +7,28 @@ function WaterTile() {
 WaterTile.prototype.tick = function(delta) {
   var something = Utils.getAnimationStep(this.time, C.animStep, 5) + 1;
   this.time += delta;
-  this.node.src = "gfx/water_" + this.type + something + ".png";
+  this.node.src = "gfx/" + this.type + something + ".png";
 }
 
 WaterTile.prototype.draw = function(container, x, y, world) {
     var leftmost= false;
     var rightmost = false;
-    if (x == 0 || Utils.getObjectClass(world.tiles[x-1][y]) == "FreeTile") {
+    if (x == 0 || Utils.getObjectClass(world.tiles[x-1][y]) != "WaterTile") {
       leftmost = true;
     }
-    if (x + 1 == world.tiles.length || Utils.getObjectClass(world.tiles[x+1][y]) == "FreeTile") {
+    if (x + 1 == world.tiles.length || Utils.getObjectClass(world.tiles[x+1][y]) != "WaterTile") {
       rightmost = true;
     }
     if (leftmost) {
-      this.type = "left";
+      this.type = "water_left";
     } else if (rightmost) {
-      this.type = "right";
+      this.type = "water_right";
     } else {
-      this.type = "center";
+      this.type = "water_center";
+    }
+    above_fountain = false;
+    if (y + 1 != world.tiles[0].length && Utils.getObjectClass(world.tiles[x][y + 1]) == "FountainTile" ) {
+      above_fountain = true;
     }
     for (var i=0;i<C.neighbours.length; ++i) {
       var tx = x + C.neighbours[i][0];
@@ -39,9 +43,12 @@ WaterTile.prototype.draw = function(container, x, y, world) {
       if (Utils.getObjectClass(world.tiles[tx][ty]) == "WaterTile") {
 	this.time = world.tiles[tx][ty].time;
 	break;
-	// alert("what");
       }
     }
-  name = "water_" + this.type + "1.png";
+  if (above_fountain) {
+    this.type = "fountain_above";
+  }
+  name = this.type + "1.png";
+  
   Tile.draw(this, container, x, y, name);
 }
