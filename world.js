48f93b41e2d49e3c330887f93c334b1dad550d7b
Sorin Stancu-mara <sstancumara@google.com>
2011-10-15 17:48:51 +0200

    Fix the "matrix" effect.

--- world.js
+++ world.js
@@ -86,7 +86,7 @@ World.prototype.fail = function() {
 World.prototype.removeActor = function(actor) {
   for (var i = 0; i < this.actors.length; i++) {
     if (this.actors[i] != actor) continue;
-    this.actors.splice(i, i + 1);
+    this.actors.splice(i, 1);
     if (actor.node) {
       actor.node.parentElement.removeChild(actor.node);
     }
