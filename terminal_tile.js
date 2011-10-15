68a9556c1335b296f54199fc5b1b97071343aed7
Ivo Strandzhev <istrandjev@google.com>
2011-10-15 12:08:07 +0200

    Add the first animated tile - the terminal tile.

---
+++ terminal_tile.js
@@ -0,0 +1,15 @@
+function TerminalTile() {
+  this.passible = false;
+  this.time = Math.random();
+}
+
+TerminalTile.prototype.tick = function(delta) {
+  var something = Utils.getAnimationStep(this.time, C.animStep, 8) + 1;
+  this.time += delta;
+  this.node.src = "gfx/terminal" + something + ".png";
+}
+
+TerminalTile.prototype.draw = function(container, x, y, world) {
+  name = "terminal1.png";
+  Tile.draw(this, container, x, y, name);
+}
