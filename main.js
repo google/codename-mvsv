889b8b6a50f44a2274aac52ea1700046eb24d9a2
Ivo Strandzhev <istrandjev@google.com>
2011-10-16 11:15:39 +0200

    Implement the trap tile

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var includes = ['constants.js', 'world.js', 'world_creator.js', 'tile.js', 'utils.js',
                 'wooden_tile.js', 'ground_tile.js', 'terminal_tile.js', 'fire.js',
                 'frost.js', 'fireball.js', 'exit_tile.js', 'snowball.js',
-                'stone_tile.js', 'water_tile.js', 'fountain.js',
+                'stone_tile.js', 'water_tile.js', 'fountain.js', 'trap_tile.js',
                 'free_tile.js', 'magician.js', 'scientist.js', 'player.js',
                 'actor.js', 'dynamite.js'];
 
---
+++ trap_tile.js
@@ -0,0 +1,65 @@
+function TrapTile() {
+  this.passible = true;
+  this.frost_ = new Frost();
+  this.melting = false;
+  this.world = null;
+  this.time = Math.random();
+  this.frozen = false;
+}
+
+TrapTile.prototype.tick = function(delta) {
+  this.time += delta;
+  if (this.frost_.finished && this.frost_.freezing) {
+    this.frost_.stopFreezing();
+  }
+  if (this.frost_.finishedMelting && this.fire_.melting) {
+    this.fire_.stopMelting();
+  }
+  if (!this.frozen) {
+    var something = Utils.getAnimationStep(this.time, C.animStep, 8) + 1;
+    this.node.src = "gfx/spike" + something + ".png";
+  } else {
+    this.node.src = "gfx/spike-frozen.png";
+  }
+    
+  this.frost_.tick(delta, this.x, this.y, this.world);
+}
+
+TrapTile.prototype.draw = function(container, x, y, world) {
+  Tile.draw(this, container, x, y, "spike1.png");
+  this.world = world
+}
+
+TrapTile.prototype.actorAction = function(actor) {
+  if (this.frozen) {
+    return;
+  }
+  var name = Utils.getObjectClass(actor);
+  if (name != "Scientist" && name != "Magician") return;
+  actor.die();
+}
+
+TrapTile.prototype.freeze = function() {
+  if (this.frozen) {
+    return;
+  }
+  this.passible = true;
+  this.frozen = true;
+  this.node.src = "gfx/spike-frozen.png";
+  this.frost_.startFreezing();
+}
+
+TrapTile.prototype.fire = function() {
+  this.melt();
+}
+
+TrapTile.prototype.melt = function() {
+  if (!this.frozen) {
+    return true;
+  }
+  this.frost_.startMelting();
+  this.frozen = false;
+  this.passible = true;
+  var something = Utils.getAnimationStep(this.time, C.animStep, 5) + 1;
+  this.node.src = "gfx/spike1.png";
+}
