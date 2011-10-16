6fcf9a57174074c3b29c40bb3c485e1c67a19c63
Sorin Stancu-mara <sstancumara@google.com>
2011-10-16 15:27:35 +0200

    Remove flushing sounds.

--- fountain.js
+++ fountain.js
@@ -5,11 +5,9 @@ function FountainTile() {
   this.x = 0;
   this.y = 0;
   this.world = null;
-  this.bgMusic = new Audio('sfx/water.mp3');
-  this.bgMusic.play();
-  this.bgMusic.loop = true;
+  FountainTile.bgMusic.play();
 }
-
+FountainTile.bgMusic = new Audio('sfx/water.mp3');
 FountainTile.prototype.draw = function(container, x, y, world) {
   this.x = x;
   this.y = y;
